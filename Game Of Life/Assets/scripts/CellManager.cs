84f44c5d32e4192c3e933626ea79d59bb120f5f6
Sam <samuel.ogg01@gmail.com>
2021-08-08 18:03:21 +0100

    Added project

---
+++ Game Of Life/Assets/scripts/CellManager.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+//using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CellManager : MonoBehaviour
+{
+    public bool state;
+    public Sprite sprite_alive;
+    public Sprite sprite_dead;
+    public SpriteRenderer spriteRenderer;
+    int count = 0;
+    public Sprite next_sprite;
+
+    List <GameObject> currentCollisions = new List <GameObject>();
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartState other_script = GameObject.Find("GridHolder").GetComponent<StartState>();
+        int index = Array.IndexOf(other_script.cells, this.gameObject);
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        currentCollisions.Add(collision.gameObject);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (spriteRenderer.sprite == sprite_alive)
+            state = true;
+        else
+            state = false; 
+    }
+
+
+    private void OnMouseDown()
+    {
+        StartState other_script = GameObject.Find("GridHolder").GetComponent<StartState>();
+        if (other_script.started == false)
+        {
+            /*for (int i = 0; i < currentCollisions.Count; i++)
+            {
+                if (currentCollisions[i].GetComponent<CellManager>().state == true)
+                {
+                    count += 1;
+                }
+            }
+            count = 0;*/
+            
+
+            this.gameObject.GetComponent<CellManager>().state = !this.gameObject.GetComponent<CellManager>().state;
+
+            if (spriteRenderer.sprite == sprite_alive)
+            {
+                spriteRenderer.sprite = sprite_dead;
+            }
+            else
+            {
+                spriteRenderer.sprite = sprite_alive;
+            }
+        }
+        count = 0;
+    }
+
+
+
+    public void NextGeneration()
+    {
+        StartState other_script = GameObject.Find("GridHolder").GetComponent<StartState>();
+        int index = Array.IndexOf(other_script.cells, this.gameObject);
+        count = 0;
+        
+
+        for (int i = 0; i < currentCollisions.Count; i++)
+        {
+            if (currentCollisions[i].GetComponent<CellManager>().state == true)
+            {
+                count += 1;
+            }
+        }
+
+
+        if ((state == true) && (count < 2 || count > 3))
+        {
+            //state = false;
+            next_sprite = sprite_dead;
+        }
+
+        else if ((count == 3) && (state == false))
+        {
+            //state = true;
+            next_sprite = sprite_alive;
+        }
+
+        else
+        {
+            next_sprite = spriteRenderer.sprite;
+        }
+    }
+}
---
+++ Game Of Life/Assets/scripts/StartState.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using Unity.Mathematics;
+using UnityEditor;
+using UnityEditorInternal;
+using UnityEngine;
+
+public class StartState : MonoBehaviour
+{
+    public static int cols = 41;
+    public static int rows = 20;
+    public float start_x = -9.95f; 
+    public float start_y = 4.74f;
+
+    float randCell;
+
+    public GameObject cell;
+    public Sprite sprite_alive;
+    public Sprite sprite_dead;
+
+    public int alive_percentage;
+    public float delay;
+
+    public GameObject[] cells = new GameObject[cols * rows];
+    int array_pos = 0;
+    GameObject new_cell;
+
+
+    public bool started = false;
+    bool simulate = false;
+    float nextGen;
+    public bool done;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    { 
+
+        for (int col = 0; col < cols; col++)
+        {
+            for (int row = 0; row < rows; row++)
+            {
+                randCell = UnityEngine.Random.Range(0, 100);
+                if (randCell < alive_percentage)
+                {
+                    cell.GetComponent<SpriteRenderer>().sprite = sprite_alive;
+                    cell.GetComponent<CellManager>().state = true;
+                    new_cell = Instantiate(cell, new Vector2((start_x + (float)col / 2), (start_y - (float)row / 2)), Quaternion.identity);
+                    new_cell.name += array_pos;
+                }
+                else
+                {
+                    cell.GetComponent<SpriteRenderer>().sprite = sprite_dead;
+                    cell.GetComponent<CellManager>().state = false;
+                    new_cell = Instantiate(cell, new Vector2((start_x + (float)col / 2), (start_y - (float)row / 2)), Quaternion.identity);
+                    new_cell.name += array_pos;
+                }
+
+                cells[array_pos] = new_cell;
+                array_pos += 1;
+            }
+        }
+    }
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("return"))
+        {
+            Debug.Log("STARTED");
+            started = true;
+        }
+
+
+
+        if (started == true)
+        {
+            if (Input.GetKeyDown("space"))
+            {
+                simulate = !simulate;
+                Debug.Log("simulate: " + simulate);
+            }
+
+
+            if (simulate && Time.time > nextGen)
+            {
+                NextGeneration();
+                
+                nextGen = Time.time + delay;
+            }
+
+            else if (Input.GetMouseButtonDown(0))
+            {
+                NextGeneration();
+            }
+        }
+    }
+
+
+    void NextGeneration()
+    {
+        for (int i = 0; i < cells.Count(); i++)
+        {
+            cells[i].GetComponent<CellManager>().NextGeneration();
+        }
+
+        for (int i = 0; i < cells.Count(); i++)
+        {
+            cells[i].GetComponent<CellManager>().spriteRenderer.sprite = cells[i].GetComponent<CellManager>().next_sprite;
+        }
+        
+    }
+
+
+}
